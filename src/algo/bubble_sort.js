551ad89f4639a612d46d1fec939d923acac0db8f
Marcio Mendes <marcio.souza@luizalabs.com>
2020-07-18 16:12:06 -0300

    feat: add simple graph to the screen

---
+++ src/algo/bubble_sort.js
@@ -0,0 +1,15 @@
+export default function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < (arr.length - i - 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+
+        console.log("i,j", arr[i], arr[j]);
+      }
+    }
+  }
+
+  return arr;
+}
