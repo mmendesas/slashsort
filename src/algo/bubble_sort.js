0c131f0ec3884f08bcffe22aa79a163fe9015938
Marcio Mendes <mmendes.as@gmail.com>
2020-07-19 15:19:27 -0300

    feat: add swap animations

--- src/algo/bubble_sort.js
+++ src/algo/bubble_sort.js
@@ -1,15 +1,16 @@
 export default function bubbleSort(arr) {
+  arr = arr.slice();
+  const interactions = [];
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < (arr.length - i - 1); j++) {
       if (arr[j] > arr[j + 1]) {
+        interactions.push([j, j + 1]);
+        // console.log("ij", j, j + 1, 'grosa', arr[i], arr[j + 1]);
         const temp = arr[j + 1];
         arr[j + 1] = arr[j];
         arr[j] = temp;
-
-        console.log("i,j", arr[i], arr[j]);
       }
     }
   }
-
-  return arr;
+  return [arr, interactions];
 }
