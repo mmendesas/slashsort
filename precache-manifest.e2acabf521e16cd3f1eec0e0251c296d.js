288ddee6982817b894217cb07ade4a5f08a6bf99
Marcio Mendes <marcio.souza@luizalabs.com>
2020-07-26 19:37:17 -0300

    Updates

---
+++ precache-manifest.e2acabf521e16cd3f1eec0e0251c296d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c33347ada0972ad1cce3bc7102cdf714",
+    "url": "/slashsort/index.html"
+  },
+  {
+    "revision": "e83aa2a62c7b94232974",
+    "url": "/slashsort/static/css/main.e9b941d7.chunk.css"
+  },
+  {
+    "revision": "8703426b12a23a68e452",
+    "url": "/slashsort/static/js/2.f1e46694.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/slashsort/static/js/2.f1e46694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83aa2a62c7b94232974",
+    "url": "/slashsort/static/js/main.72c88dba.chunk.js"
+  },
+  {
+    "revision": "622e8d7dece1525bc3bc",
+    "url": "/slashsort/static/js/runtime-main.803b0f94.js"
+  }
+]);
